45b7a9bf33117d86b3f315302495f0de9268c236
ManuCas <manucastle@hotmail.com>
2019-03-07 21:32:08 -0600

    css style added, repeated letter validaton added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ var abc = "abcdefghijklmnopqrstuvwxyz";
         var i = 0;
         var wins = 0;
         var losses = 0;
-        var usertr = "";
+        var usertr = [];
         var x = 0;
       
        
@@ -12,7 +12,7 @@ var abc = "abcdefghijklmnopqrstuvwxyz";
         function getLetter() {
             x = Math.floor(Math.random() * 26);
             compLetter = abc.charAt(x);
-            usertr = "";
+            usertr = [];
             i = 10;
             console.log("compu:" + compLetter);
             document.getElementById("gleft-txt").innerHTML = "Gueses left: " + i;
@@ -23,16 +23,16 @@ var abc = "abcdefghijklmnopqrstuvwxyz";
         document.onkeyup = function (event) {
             var user = event.key;
 
-         
+         if (!usertr.includes(user) && !/[^a-zA-Z]/.test(event.key)){
                 if (user === compLetter) {
                     wins++;
                     
                     getLetter();
                 }
 
-                else if(i>1){
-
-                    usertr = (usertr + user + ", ");
+                else if(i>1 ){
+                    usertr.push(user);
+                 //   usertr = (usertr + user + ", ");
                     i--;
                     }
                     else{
@@ -44,6 +44,7 @@ var abc = "abcdefghijklmnopqrstuvwxyz";
             document.getElementById("wins-txt").innerHTML = "Wins: " + wins;
             document.getElementById("losses-txt").innerHTML = "Losses: " + losses;
             document.getElementById("gleft-txt").innerHTML = "Gueses left: " + i;
-            document.getElementById("usertr-txt").innerHTML = "Letters guessed so far: " + usertr;
+            document.getElementById("usertr-txt").innerHTML = "Letters guessed so far: " + usertr.join(",");
+                }
         }
         
